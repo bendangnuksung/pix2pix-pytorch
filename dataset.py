b7dab1754f938e60641551b835a6801967dfccfd
bendangnuksung <bendangnuksungimsong@gmail.com>
2020-06-12 17:38:08 +0530

    change in train structure

--- dataset.py
+++ dataset.py
@@ -6,12 +6,14 @@ from PIL import Image
 import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 from utils import is_image_file, load_img
 
 
 class DatasetFromFolder(data.Dataset):
-    def __init__(self, image_dir, direction):
+    def __init__(self, image_dir, direction, input_shape=256):
         super(DatasetFromFolder, self).__init__()
         self.direction = direction
         self.a_path = join(image_dir, "a")
@@ -22,19 +24,21 @@ class DatasetFromFolder(data.Dataset):
                           transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 
         self.transform = transforms.Compose(transform_list)
+        self.input_shape = input_shape
+        self.input_shape_pad = int(self.input_shape * 1.1171875) # if input shape = 256 then input_shape_pad = 256 + 30
 
     def __getitem__(self, index):
         a = Image.open(join(self.a_path, self.image_filenames[index])).convert('RGB')
         b = Image.open(join(self.b_path, self.image_filenames[index])).convert('RGB')
-        a = a.resize((286, 286), Image.BICUBIC)
-        b = b.resize((286, 286), Image.BICUBIC)
+        a = a.resize((self.input_shape_pad, self.input_shape_pad), Image.BICUBIC)
+        b = b.resize((self.input_shape_pad, self.input_shape_pad), Image.BICUBIC)
         a = transforms.ToTensor()(a)
         b = transforms.ToTensor()(b)
-        w_offset = random.randint(0, max(0, 286 - 256 - 1))
-        h_offset = random.randint(0, max(0, 286 - 256 - 1))
+        w_offset = random.randint(0, max(0, self.input_shape_pad - self.input_shape - 1))
+        h_offset = random.randint(0, max(0, self.input_shape_pad - self.input_shape - 1))
     
-        a = a[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
-        b = b[:, h_offset:h_offset + 256, w_offset:w_offset + 256]
+        a = a[:, h_offset:h_offset + self.input_shape, w_offset:w_offset + self.input_shape]
+        b = b[:, h_offset:h_offset + self.input_shape, w_offset:w_offset + self.input_shape]
     
         a = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(a)
         b = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(b)
@@ -52,3 +56,58 @@ class DatasetFromFolder(data.Dataset):
 
     def __len__(self):
         return len(self.image_filenames)
+
+
+class DatasetFromImages(data.Dataset):
+    def __init__(self, A_images, B_images, direction, is_cv2_image=False, input_shape=256):
+        super(DatasetFromImages, self).__init__()
+        self.direction = direction
+        self.A_images = A_images if not is_cv2_image else self.cvt_to_pil(A_images)
+        self.B_images = B_images if not is_cv2_image else self.cvt_to_pil(B_images)
+
+        transform_list = [transforms.ToTensor(),
+                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+
+        self.transform = transforms.Compose(transform_list)
+        self.input_shape = input_shape
+        self.input_shape_pad = int(self.input_shape * 1.1171875) # if input shape = 256 then input_shape_pad = 256 + 30
+
+
+    def cvt_to_pil(self, images):
+        final_images = []
+        for image in images:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(image)
+            final_images.append(image)
+        return final_images
+
+
+    def __getitem__(self, index):
+        a = self.A_images[index]
+        b = self.B_images[index]
+        a = a.resize((self.input_shape_pad, self.input_shape_pad), Image.BICUBIC)
+        b = b.resize((self.input_shape_pad, self.input_shape_pad), Image.BICUBIC)
+        a = transforms.ToTensor()(a)
+        b = transforms.ToTensor()(b)
+        w_offset = random.randint(0, max(0, self.input_shape_pad - self.input_shape - 1))
+        h_offset = random.randint(0, max(0, self.input_shape_pad - self.input_shape - 1))
+    
+        a = a[:, h_offset:h_offset + self.input_shape, w_offset:w_offset + self.input_shape]
+        b = b[:, h_offset:h_offset + self.input_shape, w_offset:w_offset + self.input_shape]
+    
+        a = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(a)
+        b = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(b)
+
+        if random.random() < 0.5:
+            idx = [i for i in range(a.size(2) - 1, -1, -1)]
+            idx = torch.LongTensor(idx)
+            a = a.index_select(2, idx)
+            b = b.index_select(2, idx)
+
+        if self.direction == "a2b":
+            return a, b
+        else:
+            return b, a
+
+    def __len__(self):
+        return len(self.A_images)
--- train.py
+++ train.py
@@ -11,139 +11,279 @@ import torch.backends.cudnn as cudnn
 
 from networks import define_G, define_D, GANLoss, get_scheduler, update_learning_rate
 from data import get_training_set, get_test_set
+from dataset import DatasetFromImages
 
-# Training settings
-parser = argparse.ArgumentParser(description='pix2pix-pytorch-implementation')
-parser.add_argument('--dataset', required=True, help='facades')
-parser.add_argument('--batch_size', type=int, default=1, help='training batch size')
-parser.add_argument('--test_batch_size', type=int, default=1, help='testing batch size')
-parser.add_argument('--direction', type=str, default='b2a', help='a2b or b2a')
-parser.add_argument('--input_nc', type=int, default=3, help='input image channels')
-parser.add_argument('--output_nc', type=int, default=3, help='output image channels')
-parser.add_argument('--ngf', type=int, default=64, help='generator filters in first conv layer')
-parser.add_argument('--ndf', type=int, default=64, help='discriminator filters in first conv layer')
-parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count')
-parser.add_argument('--niter', type=int, default=100, help='# of iter at starting learning rate')
-parser.add_argument('--niter_decay', type=int, default=100, help='# of iter to linearly decay learning rate to zero')
-parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
-parser.add_argument('--lr_policy', type=str, default='lambda', help='learning rate policy: lambda|step|plateau|cosine')
-parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-parser.add_argument('--cuda', action='store_true', help='use cuda?')
-parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
-parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
-parser.add_argument('--lamb', type=int, default=10, help='weight on L1 term in objective')
-opt = parser.parse_args()
 
-print(opt)
+class MyConfig():
 
-if opt.cuda and not torch.cuda.is_available():
-    raise Exception("No GPU found, please run without --cuda")
+    def __init__(self):
+        self.dataset = 'facades'  # default dataset
+        self.batch_size = 1  # 'training batch size'
+        self.test_batch_size = 1  # testing batch size
+        self.direction = 'b2a'  # a2b or b2a
+        self.epoch_count = 1  # the starting epoch count
+        self.niter = 100  # # of iter at starting learning rate
+        self.niter_decay = 100  # '# of iter to linearly decay learning rate to zero'
+        self.input_nc = 3 # input image channels
+        self.output_nc = 3 # output image channels
+        self.ngf = 64  # generator filters in first conv layer
+        self.ndf = 64 # discriminator filters in first conv layer
+        self.lr = 0.0002  #initial learning rate for adam
+        self.lr_policy = 'lambda' # learning rate policy: lambda|step|plateau|cosine
+        self.lr_decay_iters = 50  # multiply by a gamma every lr_decay_iters iterations
+        self.beta1 = 0.5  # beta1 for adam. default=0.5        
+        self.threads = 4  # number of threads for data loader to use
+        self.seed = 77  # random seed
+        self.lamb = 10  #weight on L1 term in objective
+        self.cuda = True  # use cuda?
+        self.cuda_n = 0  # for multiple gpu
+        self.input_shape = 256  #  input shape width=height=256
 
-cudnn.benchmark = True
+    def display(self):
+        print("************************** Given config **************************")
+        for key, val in self.__dict__.items():
+            print(f'{key}\t:{val}')
+        print("*"*60)
 
-torch.manual_seed(opt.seed)
-if opt.cuda:
-    torch.cuda.manual_seed(opt.seed)
 
-print('===> Loading datasets')
-root_path = "dataset/"
-train_set = get_training_set(root_path + opt.dataset, opt.direction)
-test_set = get_test_set(root_path + opt.dataset, opt.direction)
-training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batch_size, shuffle=True)
-testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.test_batch_size, shuffle=False)
+default_config = MyConfig()
 
-device = torch.device("cuda:0" if opt.cuda else "cpu")
 
-print('===> Building models')
-net_g = define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
-net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
+def set_device(config):
+    map_location = 'cpu'
+    if not torch.cuda.is_available():
+        raise Exception("No GPU found, set config.cuda=False to use CPU")    
 
-criterionGAN = GANLoss().to(device)
-criterionL1 = nn.L1Loss().to(device)
-criterionMSE = nn.MSELoss().to(device)
+    if config.cuda:
+        device = torch.device('cuda:' + str(config.cuda_n))
+    else:
+        device = torch.device('cpu')
+    return device
 
-# setup optimizer
-optimizer_g = optim.Adam(net_g.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-optimizer_d = optim.Adam(net_d.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-net_g_scheduler = get_scheduler(optimizer_g, opt)
-net_d_scheduler = get_scheduler(optimizer_d, opt)
 
-for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
-    # train
-    for iteration, batch in enumerate(training_data_loader, 1):
-        # forward
-        real_a, real_b = batch[0].to(device), batch[1].to(device)
-        fake_b = net_g(real_a)
+def set_seed(config):
+    torch.manual_seed(config.seed)
+    if config.cuda:
+        torch.cuda.manual_seed(config.seed)
 
-        ######################
-        # (1) Update D network
-        ######################
 
-        optimizer_d.zero_grad()
+def train_from_images(A_images, B_images, A_test_images=None, B_test_images=None, is_cv2_image=False, model_ckpt_path='checkpoint/', config=default_config):
+    config.display()
+    device = set_device(config)
+    set_seed(config)
+    cudnn.benchmark = True
+    train_set = DatasetFromImages(A_images, B_images, config.direction, is_cv2_image, config.input_shape)
+    training_data_loader = DataLoader(dataset=train_set, num_workers=config.threads, batch_size=config.batch_size, shuffle=True)
+
+    test_set, testing_data_loader = None, None
+    if A_test_images is not None:
+        test_set = DatasetFromImages(A_test_images, B_test_images, config.direction, is_cv2_image, config.input_shape)
+        testing_data_loader = DataLoader(dataset=test_set, num_workers=config.threads, batch_size=config.test_batch_size, shuffle=False)
+
+    net_g = define_G(config.input_nc, config.output_nc, config.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
+    net_d = define_D(config.input_nc + config.output_nc, config.ndf, 'basic', gpu_id=device)
+
+    criterionGAN = GANLoss().to(device)
+    criterionL1  = nn.L1Loss().to(device)
+    criterionMSE = nn.MSELoss().to(device)
+
+    optimizer_g = optim.Adam(net_g.parameters(), lr=config.lr, betas=(config.beta1, 0.999))
+    optimizer_d = optim.Adam(net_d.parameters(), lr=config.lr, betas=(config.beta1, 0.999))
+    net_g_scheduler = get_scheduler(optimizer_g, config)
+    net_d_scheduler = get_scheduler(optimizer_d, config)
+
+    for epoch in range(config.epoch_count, config.niter + config.niter_decay + 1):
+        # train
+        for iteration, batch in enumerate(training_data_loader, 1):
+            # forward
+            real_a, real_b = batch[0].to(device), batch[1].to(device)
+
+            fake_b = net_g(real_a)
+            ######################
+            # (1) Update D network
+            ######################
+            optimizer_d.zero_grad()
         
-        # train with fake
-        fake_ab = torch.cat((real_a, fake_b), 1)
-        pred_fake = net_d.forward(fake_ab.detach())
-        loss_d_fake = criterionGAN(pred_fake, False)
+            # train with fake
+            fake_ab = torch.cat((real_a, fake_b), 1)
+            pred_fake = net_d.forward(fake_ab.detach())
+            loss_d_fake = criterionGAN(pred_fake, False)
 
-        # train with real
-        real_ab = torch.cat((real_a, real_b), 1)
-        pred_real = net_d.forward(real_ab)
-        loss_d_real = criterionGAN(pred_real, True)
+            # train with real
+            real_ab = torch.cat((real_a, real_b), 1)
+            pred_real = net_d.forward(real_ab)
+            loss_d_real = criterionGAN(pred_real, True)
+            
+            # Combined D loss
+            loss_d = (loss_d_fake + loss_d_real) * 0.5
+
+            loss_d.backward()
+           
+            optimizer_d.step()
+
+            ######################
+            # (2) Update G network
+            ######################
+
+            optimizer_g.zero_grad()
+
+            # First, G(A) should fake the discriminator
+            fake_ab = torch.cat((real_a, fake_b), 1)
+            pred_fake = net_d.forward(fake_ab)
+            loss_g_gan = criterionGAN(pred_fake, True)
+
+            # Second, G(A) = B
+            loss_g_l1 = criterionL1(fake_b, real_b) * config.lamb
+            
+            loss_g = loss_g_gan + loss_g_l1
+            
+            loss_g.backward()
+
+            optimizer_g.step()
+
+            if iteration % 25 == 0 :
+                print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f}".format(
+                    epoch, iteration, len(training_data_loader), loss_d.item(), loss_g.item()))
+
+        update_learning_rate(net_g_scheduler, optimizer_g)
+        update_learning_rate(net_d_scheduler, optimizer_d)
+
+        # test
+        if testing_data_loader is not None:
+            avg_psnr = 0
+            for batch in testing_data_loader:
+                input, target = batch[0].to(device), batch[1].to(device)
+
+                prediction = net_g(input)
+                mse = criterionMSE(prediction, target)
+                psnr = 10 * log10(1 / mse.item())
+                avg_psnr += psnr
+            print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+        #checkpoint
+        if epoch % 50 == 0:
+            if not os.path.exists(model_ckpt_path):
+                os.mkdir(model_ckpt_path)
+            net_g_model_out_path = f"{model_ckpt_path}/netG_model_epoch_{epoch}.pth"
+            net_d_model_out_path = f"{model_ckpt_path}/netD_model_epoch_{epoch}.pth"
+            torch.save(net_g, net_g_model_out_path)
+            torch.save(net_d, net_d_model_out_path)
+            print("Checkpoint saved to -- ", config.dataset)
+
+
+
+
+
+
+
+# if opt.cuda and not torch.cuda.is_available():
+#     raise Exception("No GPU found, please run without --cuda")
+
+
+
+# torch.manual_seed(opt.seed)
+# if opt.cuda:
+#     torch.cuda.manual_seed(opt.seed)
+
+
+# print('===> Loading datasets')
+# root_path = "dataset/"
+# train_set = get_training_set(root_path + opt.dataset, opt.direction)
+# test_set = get_test_set(root_path + opt.dataset, opt.direction)
+# training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batch_size, shuffle=True)
+# testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.test_batch_size, shuffle=False)
+
+# device = torch.device("cuda:0" if opt.cuda else "cpu")
+
+# print('===> Building models')
+# net_g = define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
+# net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
+
+# criterionGAN = GANLoss().to(device)
+# criterionL1 = nn.L1Loss().to(device)
+# criterionMSE = nn.MSELoss().to(device)
+
+# # setup optimizer
+# optimizer_g = optim.Adam(net_g.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+# optimizer_d = optim.Adam(net_d.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+# net_g_scheduler = get_scheduler(optimizer_g, opt)
+# net_d_scheduler = get_scheduler(optimizer_d, opt)
+
+# for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+#     # train
+#     for iteration, batch in enumerate(training_data_loader, 1):
+#         # forward
+#         real_a, real_b = batch[0].to(device), batch[1].to(device)
+#         fake_b = net_g(real_a)
+
+#         ######################
+#         # (1) Update D network
+#         ######################
+
+#         optimizer_d.zero_grad()
         
-        # Combined D loss
-        loss_d = (loss_d_fake + loss_d_real) * 0.5
+#         # train with fake
+#         fake_ab = torch.cat((real_a, fake_b), 1)
+#         pred_fake = net_d.forward(fake_ab.detach())
+#         loss_d_fake = criterionGAN(pred_fake, False)
 
-        loss_d.backward()
+#         # train with real
+#         real_ab = torch.cat((real_a, real_b), 1)
+#         pred_real = net_d.forward(real_ab)
+#         loss_d_real = criterionGAN(pred_real, True)
+        
+#         # Combined D loss
+#         loss_d = (loss_d_fake + loss_d_real) * 0.5
+
+#         loss_d.backward()
        
-        optimizer_d.step()
+#         optimizer_d.step()
 
-        ######################
-        # (2) Update G network
-        ######################
+#         ######################
+#         # (2) Update G network
+#         ######################
 
-        optimizer_g.zero_grad()
+#         optimizer_g.zero_grad()
 
-        # First, G(A) should fake the discriminator
-        fake_ab = torch.cat((real_a, fake_b), 1)
-        pred_fake = net_d.forward(fake_ab)
-        loss_g_gan = criterionGAN(pred_fake, True)
+#         # First, G(A) should fake the discriminator
+#         fake_ab = torch.cat((real_a, fake_b), 1)
+#         pred_fake = net_d.forward(fake_ab)
+#         loss_g_gan = criterionGAN(pred_fake, True)
 
-        # Second, G(A) = B
-        loss_g_l1 = criterionL1(fake_b, real_b) * opt.lamb
+#         # Second, G(A) = B
+#         loss_g_l1 = criterionL1(fake_b, real_b) * opt.lamb
         
-        loss_g = loss_g_gan + loss_g_l1
+#         loss_g = loss_g_gan + loss_g_l1
         
-        loss_g.backward()
+#         loss_g.backward()
 
-        optimizer_g.step()
+#         optimizer_g.step()
 
-        print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f}".format(
-            epoch, iteration, len(training_data_loader), loss_d.item(), loss_g.item()))
+#         print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f}".format(
+#             epoch, iteration, len(training_data_loader), loss_d.item(), loss_g.item()))
 
-    update_learning_rate(net_g_scheduler, optimizer_g)
-    update_learning_rate(net_d_scheduler, optimizer_d)
+#     update_learning_rate(net_g_scheduler, optimizer_g)
+#     update_learning_rate(net_d_scheduler, optimizer_d)
 
-    # test
-    avg_psnr = 0
-    for batch in testing_data_loader:
-        input, target = batch[0].to(device), batch[1].to(device)
+#     # test
+#     avg_psnr = 0
+#     for batch in testing_data_loader:
+#         input, target = batch[0].to(device), batch[1].to(device)
 
-        prediction = net_g(input)
-        mse = criterionMSE(prediction, target)
-        psnr = 10 * log10(1 / mse.item())
-        avg_psnr += psnr
-    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+#         prediction = net_g(input)
+#         mse = criterionMSE(prediction, target)
+#         psnr = 10 * log10(1 / mse.item())
+#         avg_psnr += psnr
+#     print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
 
-    #checkpoint
-    if epoch % 50 == 0:
-        if not os.path.exists("checkpoint"):
-            os.mkdir("checkpoint")
-        if not os.path.exists(os.path.join("checkpoint", opt.dataset)):
-            os.mkdir(os.path.join("checkpoint", opt.dataset))
-        net_g_model_out_path = "checkpoint/{}/netG_model_epoch_{}.pth".format(opt.dataset, epoch)
-        net_d_model_out_path = "checkpoint/{}/netD_model_epoch_{}.pth".format(opt.dataset, epoch)
-        torch.save(net_g, net_g_model_out_path)
-        torch.save(net_d, net_d_model_out_path)
-        print("Checkpoint saved to {}".format("checkpoint" + opt.dataset))
+#     #checkpoint
+#     if epoch % 50 == 0:
+#         if not os.path.exists(model_ckpt_path):
+#             os.mkdir(model_ckpt_path)
+#         if not os.path.exists(os.path.join(model_ckpt_path, opt.dataset)):
+#             os.mkdir(os.path.join(model_ckpt_path, opt.dataset))
+#         net_g_model_out_path = f"{model_ckpt_path}/{}/netG_model_epoch_{}.pth".format(opt.dataset, epoch)
+#         net_d_model_out_path = f"{model_ckpt_path}/{}/netD_model_epoch_{}.pth".format(opt.dataset, epoch)
+#         torch.save(net_g, net_g_model_out_path)
+#         torch.save(net_d, net_d_model_out_path)
+#         print("Checkpoint saved to {}".format("checkpoint" + opt.dataset))
